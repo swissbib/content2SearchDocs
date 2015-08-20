118ab349b955cae0046fc80009a89e9d8feb29db
Oliver Schihin <oliver.schihin@unibas.ch>
2015-08-20 15:37:20 +0200

    Konfiguration und Skripte für Indexierung von Archivdaten

--- shellscripts/sb_post2solr_archivaldata.sh
+++ shellscripts/sb_post2solr_archivaldata.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
-#
-# oliver.schihin@unibas.ch / 21.11.2011
-#erweitert: Guenter Hipler 3.5.2012
 
+# sb_post2solr_archivaldata.sh
+# ****************************
+# Description: Script posts search engine documents to the solr index specified on the command line
+#              Collections concerned are not updated through frequent and automatic procedures but treated completely.
+#              For re-indexing, delete all records first from index
+#
+# Authors: Günter Hipler (ghi), Oliver Schihin (osc)
+#
+# History
+# 21.11.2011 : osc : Creation (generic version)
+# 03.05.2012 : ghi : Extended
+# 20.08.2015 : osc : Adaption to new setup of archival data indexing
+#
+# Generic path definitions and variables
 PROJECTDIR_DOCPROCESSING=/swissbib_index/solrDocumentProcessing/MarcToSolr
 LOGDIR=${PROJECTDIR_DOCPROCESSING}/data/log
-
-
 CURRENT_TIMESTAMP=`date +%Y%m%d%H%M%S`
-LOGFILE=$LOGDIR/InitialPostingEAD.$CURRENT_TIMESTAMP.log
-POSTDIRBASE=${PROJECTDIR_DOCPROCESSING}/data/outputfiles.nbead
+LOGFILE=$LOGDIR/post2solrArchivaldata.$CURRENT_TIMESTAMP.log
+POSTDIRBASE=${PROJECTDIR_DOCPROCESSING}/data/outputfiles_archivaldata
 POSTJAR=${PROJECTDIR_DOCPROCESSING}/dist/post.jar
 
-
-
-
 #ulimit -v unlimited
-
-
 
 function usage()
 {
@@ -29,47 +33,36 @@ function setTimestamp()
     CURRENT_TIMESTAMP=`date +%Y%m%d%H%M%S`
 }
 
-
 function preChecks()
 {
-
     printf "in preChecks ...\n" >> $LOGFILE
-
 
     if [ ! -d ${POSTDIRBASE} ]
     then
             printf "ERROR : base directory for files to be posted does not exist!\n" >>$LOGFILE && exit 9
     fi
 
-
     #-z: the length of string is zero
     [ -z  ${URLSOLRINDEXSERVER} ] && usage
 
     #-n: True if the length of "STRING" is non-zero.
     [ ! -n "${URLSOLRINDEXSERVER}" ] && echo "solr index server  is not set" >>$LOGFILE && exit 9
-       
 }
 
-
-function post2solr ()
+# Indexing function
+function post2solr()
 {
-
-
    for dir in `ls ${POSTDIRBASE}`
-    do
-
+   do
        setTimestamp
        printf "start posting to SOLR in <%s>\n" ${dir} >> ${LOGFILE}
 
        POSTDIR=${POSTDIRBASE}/$dir/*.xml
 
        java -Xms1024m -Xmx1024m \
-        -Durl=${URLSOLRINDEXSERVER} \
-        -Dcommit=no   \
-        -jar ${POSTJAR} ${POSTDIR} >> ${LOGFILE}
-
-#        rm -r $file
-
+            -Durl=${URLSOLRINDEXSERVER} \
+            -Dcommit=no   \
+            -jar ${POSTJAR} ${POSTDIR} >> ${LOGFILE}
     done
 
     printf "now send the commit to <%s> ...\n" ${URLSOLRINDEXSERVER} >> ${LOGFILE}
@@ -77,10 +70,7 @@ function post2solr ()
         -Durl=${URLSOLRINDEXSERVER} \
         -Dcommit=yes   \
         -jar ${POSTJAR}  >> ${LOGFILE}
-
-
 }
-
 
 while getopts hs: OPTION
 do
@@ -98,9 +88,9 @@ preChecks
 
 setTimestamp
 
-printf "sb_post2solr_initialloading.sh started at <%s>: ... \n" ${CURRENT_TIMESTAMP} >> ${LOGFILE}
+printf "sb_post2solr_archivaldata.sh started at <%s>: ... \n" ${CURRENT_TIMESTAMP} >> ${LOGFILE}
 
 post2solr
 
 setTimestamp
-printf "sb_post2solr_initialloading.sh finished at <%s>: ...\n" ${CURRENT_TIMESTAMP} >> ${LOGFILE}
+printf "sb_post2solr_archivaldata.sh finished at <%s>: ...\n" ${CURRENT_TIMESTAMP} >> ${LOGFILE}
