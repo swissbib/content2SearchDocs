3b8a65c47d17650c34c248005e08abd7b8664bf5
edelm <matthias.edel@unibas.ch>
2019-03-22 15:53:20 +0100

    reading libraries/cantons from local file and with new json structure

--- src/main/java/org/swissbib/documentprocessing/plugins/CreateLibraryFacets.java
+++ src/main/java/org/swissbib/documentprocessing/plugins/CreateLibraryFacets.java
@@ -3,7 +3,8 @@ package org.swissbib.documentprocessing.plugins;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import java.net.URL;
+
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -58,7 +59,8 @@ public class CreateLibraryFacets implements IDocProcPlugin {
 
         ObjectMapper mapper = new ObjectMapper();
         try {
-            libadminGeoJson = mapper.readValue(new URL(configuration.get("LIBADMIN_DEFINITIONS")), LibadminGeoJson.class);
+            // get libadminGeoJson from local file (synced by nightly cronjob)
+            libadminGeoJson = mapper.readValue(new FileInputStream(configuration.get("LIBADMIN_DEFINITIONS")), LibadminGeoJson.class);
         } catch (Exception e) {
             CreateLibraryFacets.facetLibraryLogger.error(e.getMessage() + "\\r\\n" + e);
         }
@@ -75,16 +77,15 @@ public class CreateLibraryFacets implements IDocProcPlugin {
      * @return
      */
     public String[] getHierarchicalLibraryFacet(String institutionCode) {
-        //System.out.print("received:" + institutionCode);
         String[] institutionCodeArray = institutionCode.split(",");
         List<String> hierarchyStrings = new ArrayList<String>();
         String e0, e1;
 
         for (int i=0; i< institutionCodeArray.length; i++) {
-            for (LibadminGeoJsonFeature feature : libadminGeoJson.getFeatures()) {
-                if (feature.getProperties().getBib_code().equals(institutionCodeArray[i])) {
-                    e0 =  "0/" + feature.getProperties().getCanton() + "/";
-                    e1 = "1/" + feature.getProperties().getCanton() + "/" + institutionCodeArray[i] + "/";
+            for (Institution institution : libadminGeoJson.getInstitutions()) {
+                if (institution.getBib_code().equals(institutionCodeArray[i])) {
+                    e0 =  "0/" + institution.getCanton() + "/";
+                    e1 = "1/" + institution.getCanton() + "/" + institutionCodeArray[i] + "/";
                     if (!hierarchyStrings.contains(e0)) {
                         hierarchyStrings.add(e0);
                     }
@@ -97,7 +98,7 @@ public class CreateLibraryFacets implements IDocProcPlugin {
         if (hierarchyStrings.size() == 0) hierarchyStrings.add("");
         String[] stringArray = new String[hierarchyStrings.size()];
         hierarchyStrings.toArray(stringArray);
-        //System.out.println("returning:" + Arrays.toString(stringArray));
+        System.out.println("returning:" + Arrays.toString(stringArray));
         return stringArray;
     }
 
@@ -108,63 +109,24 @@ public class CreateLibraryFacets implements IDocProcPlugin {
  */
 
 class LibadminGeoJson {
-    private String type;
-    public String getType() { return this.type; }
-    public void setType(String type) { this.type = type; }
-    private LibadminGeoJsonFeature[] features;
-    public LibadminGeoJsonFeature[] getFeatures() { return this.features; }
-    public void setFeatures(LibadminGeoJsonFeature[] features) { this.features = features; }
+    private Institution[] institutions;
+    public Institution[] getInstitutions() { return this.institutions; }
+    public void setInstitutions(Institution[] institutions) { this.institutions = institutions; }
 }
 
-class LibadminGeoJsonFeature {
-    private String type;
-    public String getType() { return this.type; }
-    public void setType(String type) { this.type = type; }
-    private Properties properties;
-    public Properties getProperties() { return this.properties; }
-    public void setProperties(Properties properties) { this.properties = properties; }
-    private Geometry geometry;
-    public Geometry getGeometry() { return this.geometry; }
-    public void setGeometry(Geometry geometry) { this.geometry = geometry; }
-}
-
-class Properties {
+class Institution {
     private String bib_code;
-    private String group_code;
-    private Address address;
-    private String canton;
-    private String website;
-    private Label group_label;
-    private Label label;
-    private Url url;
     public String getBib_code() { return this.bib_code; }
     public void setBib_code(String bib_code) { this.bib_code = bib_code; }
-    public String getGroup_code() { return this.group_code; }
-    public void setGroup_code(String group_code) { this.group_code = group_code; }
-    public Address getAddress() { return this.address; }
-    public void setAddress(Address address) { this.address = address; }
+    private String canton;
     public String getCanton() { return this.canton; }
     public void setCanton(String canton) { this.canton = canton; }
-    public Label getGroup_label() { return this.group_label; }
-    public void setGroup_label(Label group_label) { this.group_label = group_label; }
+    private String group_code;
+    public String getGroup_code() { return this.group_code; }
+    public void setGroup_code(String group_code) { this.group_code = group_code; }
+    private Label label;
     public Label getLabel() { return this.label; }
     public void setLabel(Label label) { this.label = label; }
-    public Url getUrl() { return this.url; }
-    public void setUrl(Url url) { this.url = url; }
-    public String getWebsite() { return this.website; }
-    public void setWebsite(String website) { this.website = website; }
-}
-
-class Address {
-    private String address;
-    public String getAddress() { return this.address; }
-    public void setAddress(String address) { this.address = address; }
-    private String zip;
-    public String getZip() { return this.zip; }
-    public void setZip(String zip) { this.zip = zip; }
-    private String city;
-    public String getCity() { return this.city; }
-    public void setCity(String city) { this.city = city; }
 }
 
 class Label {
@@ -180,28 +142,4 @@ class Label {
     private String en;
     public String getEn() { return this.en; }
     public void setEn(String en) { this.en = en; }
-}
-
-class Url {
-    private String de;
-    public String getDe() { return this.de; }
-    public void setDe(String de) { this.de = de; }
-    private String fr;
-    public String getFr() { return this.fr; }
-    public void setFr(String fr) { this.fr = fr; }
-    private String it;
-    public String getIt() { return this.it; }
-    public void setIt(String it) { this.it = it; }
-    private String en;
-    public String getEn() { return this.en; }
-    public void setEn(String en) { this.en = en; }
-}
-
-class Geometry {
-    private String type;
-    public String getType() { return this.type; }
-    public void setType(String type) { this.type = type; }
-    private float[] coordinates;
-    public float[] getCoordinates() { return this.coordinates; }
-    public void setCoordinates(float[] coordinates) { this.coordinates = coordinates; }
 }
