1c80a25d858b81d4cbc2b2b8bb403473e6859f33
Guenter Hipler <guenter.hipler@unibas.ch>
2019-03-26 23:34:04 +0100

    next step to include MF xslt pipes

--- src/test/java/org/swissbib/TestMFDocEngine.java
+++ src/test/java/org/swissbib/TestMFDocEngine.java
@@ -1,34 +1,45 @@
 package org.swissbib;
 
 import org.junit.jupiter.api.Test;
+import org.metafacture.io.FileOpener;
+import org.metafacture.io.LineReader;
+import org.metafacture.io.ObjectStdoutWriter;
 import org.swissbib.metafacture.xml.xslt.XSLTPipeStart;
+import org.swissbib.metafacture.xml.xslt.XSLTPipeStop;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.Scanner;
 
 public class TestMFDocEngine {
 
     @Test
-    void firstTransformationWithTemplateStep1() {
+    void firstTransformationWithTemplateStep1() throws Exception {
 
+        ClassLoader classLoader = new TestMFDocEngine().getClass().getClassLoader();
+
+        File startfile = new File(classLoader.getResource
+                ("org/swissbib/documentprocessing/step1.xsl").getFile());
+        File stopfile = new File(classLoader.getResource
+                ("org/swissbib/documentprocessing/step2.xsl").getFile());
+        //String content = new String(Files.readAllBytes(file.toPath()));
+        //System.out.println(content);
+        File datafile = new File(classLoader.getResource
+                ("org/swissbib/documentprocessing/testfile.xml.gz").getFile());
+
+        FileOpener fo = new FileOpener();
+        LineReader lr = new LineReader();
+        fo.setReceiver(lr);
         XSLTPipeStart ps = new XSLTPipeStart();
+        ps.setTemplate(startfile.getAbsolutePath());
+        lr.setReceiver(ps);
 
-        //InputStream inputStream =
-        //        Thread.currentThread().getContextClassLoader().
-        //                getResourceAsStream("org/swissbib/documentprocessing/step1.xsl");
-        try {
-            //java.net.URL url = TestMFDocEngine.class.
-            ClassLoader classLoader = new TestMFDocEngine().getClass().getClassLoader();
+        XSLTPipeStop pipeStop = new XSLTPipeStop();
+        pipeStop.setTemplate(stopfile.getAbsolutePath());
+        ObjectStdoutWriter<String> ow = new ObjectStdoutWriter<>();
+        pipeStop.setReceiver(ow);
 
-            File file = new File(classLoader.getResource
-                    ("org/swissbib/documentprocessing/step1.xsl").getFile());
-            String content = new String(Files.readAllBytes(file.toPath()));
-            System.out.println(content);
-        } catch (Exception exc) {
-            exc.printStackTrace();
-        }
+        ps.setReceiver(pipeStop);
+        fo.process(datafile.getAbsolutePath());
 
 
     }
