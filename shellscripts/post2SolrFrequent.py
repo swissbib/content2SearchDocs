4cbb8fcfd6dbb4d072800ba32c9521d7ed3ea40d
Lionel Walter <lionel.walter@unibas.ch>
2019-06-27 10:54:08 +0200

    add missing point

--- shellscripts/post2SolrFrequent.py
+++ shellscripts/post2SolrFrequent.py
@@ -154,7 +154,7 @@ class Post2SolrFrequent:
 
     def archiveAndZip(self):
         self.writeLogMessage(" documents are now archived...")
-        os.system("tar zcf " + self.ARCHIVE_DIR + os.sep + "PostedSolrDocuments" + self.currentDateTime() + "tar.gz " + self.POSTDIRBASE_TO + os.sep + "* --remove-files")
+        os.system("tar zcf " + self.ARCHIVE_DIR + os.sep + "PostedSolrDocuments" + self.currentDateTime() + ".tar.gz " + self.POSTDIRBASE_TO + os.sep + "* --remove-files")
 
 
 
